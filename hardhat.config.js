ae49f9f0b8463c073489a525e962ff09b3bc31ee
Siddharth Patel <siddharth.patel.231999@gmail.com>
2023-08-12 21:34:25 +0530

    base-goerli deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,10 +17,20 @@ module.exports = {
       url: `https://eth-sepolia.g.alchemy.com/v2/GcV0dj3ENX3ImHm5_6gcAD4ySJ7un_sT`,
       // accounts: [''],
     },
+    Base: {
+      url: `https://rpc.ankr.com/base_goerli`,
+      accounts: [''],
+    },
+    'base-goerli': {
+      url: 'https://goerli.base.org',
+      accounts: [``],
+      gasPrice: 1000000000,
+    },
   },
   etherscan: {
     apiKey: {
-      sepolia: 'I9NHWY6MEQIG9ZYJTEUWQ6UBPZFI9QSYFU'
+      // sepolia: 'I9NHWY6MEQIG9ZYJTEUWQ6UBPZFI9QSYFU',
+      "baseGoerli": `PLACEHOLDER_STRING`
     }
   },
     hardhat: {
