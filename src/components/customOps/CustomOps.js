073951b73966f9b540d7ae2e46124ed211bc79bb
Siddharth Patel <siddharth.patel.231999@gmail.com>
2023-08-12 12:32:07 +0530

    custom ops added

--- src/components/customOps/CustomOps.js
+++ src/components/customOps/CustomOps.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Window, WindowContent, TextInput, Button, Checkbox, Hourglass } from 'react95';
 import { ThemeProvider } from 'styled-components';
 import original from 'react95/dist/themes/original';
+import { ethers } from "ethers";
+import axios from 'axios';
 
+import { getProof } from "../helper/generateProof";
+import connectWallet from "../helper/connectWallet";
+import { transferETH } from "../helper/generateCalldata";
+import { generateContractParams } from "../helper/generateContractParams";
+import { transact } from "../helper/transact";
 
 
 const CustomOps = () => {
@@ -14,8 +21,90 @@ const CustomOps = () => {
   const [loading, setLoading] = React.useState(false);
   const [txnHash, setTXNHash] = React.useState("0x00");
 
+  const handleExplorerClick = () => {
+    if (txnHash === "0x00") {
+      alert("no transaction found");
+    }
+    else {
+      window.open(`https://sepolia.etherscan.io/tx/${txnHash}`, '_blank');
+    }
+  };
 
-  const handleSubmit = async () => { };
+  const haveMetamask = async () => {
+    setMetamaskStatus(!metamaskStatus);
+  };
+
+  const handleInputChange = (e) => {
+    setInputString(e.target.value);
+    handleConvertToArray();
+  };
+
+  const handleConvertToArray = () => {
+    try {
+      const array = inputString.split(',');
+      setCallData(array);
+    } catch (error) {
+      console.error('Invalid input format:', error);
+    }
+  };
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    // let wallet = await connectWallet();
+    const rpc = process.env.REACT_APP_SEPOLIA_RPC;
+    const sponsor = process.env.REACT_APP_SPONSOR;
+    const API = (process.env.REACT_APP_PAYMASTER_API).toString();
+    const provider = new ethers.providers.JsonRpcProvider(rpc.toString());
+    const network = await provider.getNetwork();
+
+    let calldata = [];
+    let proof;
+    let fakeRefundData = await transferETH(sponsor, ethers.utils.parseEther("0.000000001"));
+    calldata = [callData[0], fakeRefundData]
+
+    if (metamaskStatus) {
+      let wallet = await connectWallet();
+      proof = await getProof(password, calldata, network.chainId);
+      let txData = await generateContractParams(wallet, proof, username, calldata, password, network.chainId, await wallet.getAddress());
+      if (txData) {
+        let res = await transact(wallet, txData.proofFinal, txData.calldata, username, txData.gasPrice, txData.estimatedGas);
+        if (res) {
+          setLoading(false);
+          alert("Action Successfull");
+          setTXNHash(res);
+        } else {
+          setLoading(false);
+        }
+      } else {
+        setLoading(false);
+      }
+    }
+    else {
+      let wallet;
+      proof = await getProof(password, calldata, network.chainId);
+      let txData = await generateContractParams(wallet, proof, username, calldata, password, network.chainId, sponsor);
+      const payload =
+      {
+        proof0: txData.proofFinal[0],
+        proof1: txData.proofFinal[1],
+        proof2: txData.proofFinal[2],
+        proof3: txData.proofFinal[3],
+        username: username,
+        calldata: txData.calldata,
+        gasPrice: txData.gasPrice,
+        gasLimit: txData.estimatedGas
+    }
+      let res = await axios.post(API, payload);
+      if (res.status === 200) {
+        setLoading(false);
+        alert("Action Successfull");
+        setTXNHash(res.data.transactionHash);
+      }
+      else {
+        setLoading(false);
+      }
+    }
+  };
 
   return (
     <ThemeProvider theme={original}>
@@ -44,10 +133,11 @@ const CustomOps = () => {
               label="CallData"
               type="Calldata"
               value={callData}
+              onChange={handleInputChange}
             />
             <Button onClick={handleSubmit} style={{ width: '145px' }}>Hit</Button>
-            <Button >View it on explorer</Button>
-            <Checkbox label='metamask?' className="Toggle" checked={metamaskStatus} />
+            <Button onClick={handleExplorerClick}>View it on explorer</Button>
+            <Checkbox label='metamask?' className="Toggle" checked={metamaskStatus} onChange={haveMetamask} />
           </WindowContent>
           {loading ? (
             <div style={{ textAlign: 'center' }}>
@@ -65,3 +155,4 @@ const CustomOps = () => {
 export default CustomOps;
 
 
+//0x000000000000000000000000b4d883a737e126d1a1ced4be61d258146a8879d600000000000000000000000000000000000000000000000000038d7ea4c68000000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000
